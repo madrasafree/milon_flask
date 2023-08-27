769741395f2afce256bf637b6c3bac993776b31b
iddober <iddoberger@gmail.com>
2023-08-27 17:52:02 +0300

    add get_label_data_dicts

--- source/main.py
+++ source/main.py
@@ -2,19 +2,14 @@ from dataclasses import dataclass
 
 from flask import Flask, redirect, url_for, request, render_template
 from sqlalchemy import func
-from arabic_words_db import ArabicWordsDB, Labels, WordsLabels
+from arabic_words_db import ArabicWordsDB, Labels, WordsLabels, Words
 from includes_utils import get_top_variables, get_trailer_variables
 
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
-# """  case x>=0 AND x<=10
 
-
-
-@app.route("/labels.asp")
-def labels_handler():
-
+def get_label_data_dicts():
     with ArabicWordsDB() as arabic_words_db:
         rows = arabic_words_db.session.query(Labels.ID, Labels.labelName).all()
 
@@ -24,7 +19,7 @@ def labels_handler():
         label_name = label_row.labelName
 
         with ArabicWordsDB() as arabic_words_db:
-            word_count = arabic_words_db.session.query(func.count(WordsLabels.wordID)).\
+            word_count = arabic_words_db.session.query(func.count(WordsLabels.wordID)). \
                 filter(WordsLabels.labelID == label_id).scalar()
 
             if word_count <= 10:
@@ -40,7 +35,6 @@ def labels_handler():
             else:
                 tag_size = "2.4em"
 
-
         label_data_dict = {}
         label_data_dict["name"] = label_name
         label_data_dict["title"] = f"there are {word_count} words in this topic"
@@ -49,25 +43,44 @@ def labels_handler():
 
         label_data_dicts.append(label_data_dict)
 
+    return label_data_dicts
+
+
+@app.route("/labels.asp")
+def labels_handler():
+    label_data_dicts = get_label_data_dicts()
+
     return render_template("labels.html", label_data_dicts=label_data_dicts)
-
-
-
-#http://localhost:8081/label.asp?id=5
 
 
 @app.route("/label.asp")
 def label_handler():
     label_id = request.args.get("id")
     if not label_id:
-        label_id = "no word_id was found"
+        label_id = "no label_id was found"
 
-    all_labels = get_all_labels()
+    with ArabicWordsDB() as arabic_words_db:
+        row = arabic_words_db.session.query(Labels.labelName).filter(Labels.ID == label_id).one()
 
-    label_name = "food"
+    label_name = row.labelName
 
+    with ArabicWordsDB() as arabic_words_db:
+        rows = arabic_words_db.session.query(WordsLabels.wordID).filter(WordsLabels.labelID == label_id).all()
 
-    return render_template("label.html",  labels=all_labels, label_id=label_id, label_name=label_name)
+    all_word_ids = [row.wordID for row in rows]
+
+    with ArabicWordsDB() as arabic_words_db:
+        rows = arabic_words_db.session.query(Words).filter(Words.id.in_(all_word_ids)).all()
+
+    word_count = len(rows)
+
+    for row in rows:
+        pass
+
+    label_data_dicts = get_label_data_dicts()
+
+    return render_template("label.html", label_data_dicts=label_data_dicts, label_name=label_name,
+                           word_count=word_count)
 
 
 
