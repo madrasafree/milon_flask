d1faceae030c6cea03a70f7e952750657cde3331
tkalir <tkalir@gmail.com>
2024-07-18 22:09:36 +0300

    moving flask to container

--- db_docker/load_dumps.sh
+++ db_docker/load_dumps.sh
@@ -4,3 +4,4 @@ psql -h localhost -p 5432 -U postgres -d arabicWords -f ./db_dumps/arabicWords_s
 psql -h localhost -p 5432 -U postgres -d arabicWords -f ./db_dumps/arabicWords_data.sql
 psql -h localhost -p 5432 -U postgres -d arabicUsers -f ./db_dumps/arabicUsers_schema.sql
 psql -h localhost -p 5432 -U postgres -d arabicUsers -f ./db_dumps/arabicUsers_data.sql
+echo "dump load done"
